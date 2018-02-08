74fdfe95595464f807ee9e256dae7b7eeed1de1e
lequerica <lequerica@Martins-MacBook-Pro.local>
2018-02-08 17:08:01 -0300

    first commit

---
+++ src/main/java/ar/com/sourceSistemas/photoLocator/Main.java
@@ -0,0 +1,12 @@
+package ar.com.sourceSistemas.photoLocator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		find /Users/lequerica/Desktop/Telefono -name *.jpg 
+
+
+	}
+
+}
